531303818c2836462bfb2a014b113062d90bc5e7
Top-Slayer <dragontops55@gmail.com>
2026-01-04 00:36:20 +0700

    modify: show all files in neotree, switch keymap between neotree plugin and explore built-in

--- nvim/lua/map.lua
+++ nvim/lua/map.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.keymap.set("n", "<leader>e", vim.cmd.Explore)
+vim.keymap.set("n", "<C-e>", vim.cmd.Explore)
 
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,20 +1,37 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    keys = {
-        { "<C-e>", "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
-    },
-    opts = {
-        close_if_last_window = true,
-        window = { position = "left", width = 30 },
-        filesystem = { follow_current_file = { enabled = true } },
-    },
-  }
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons", -- optional, but recommended
+        },
+        lazy = false, -- neo-tree will lazily load itself
+        keys = {
+            { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
+        },
+        opts = {
+            close_if_last_window = true,
+            window = {
+                position = "left",
+                auto_expand_width = true,
+            },
+            filesystem = {
+                follow_current_file = {
+                    enabled = true,
+                },
+                filtered_items = {
+                    visible = true,
+                    hide_dotfiles = true,
+                    hide_gitignored = true,
+                    hide_hidden = false,
+                    hide_by_name = {
+                        -- ".DS_Store",
+                        -- "thumbs.db",
+                    },
+                }
+            },
+        },
+    }
 }
