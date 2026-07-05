52f4faa508894b0cfd9c90926d6770e00b941ca3
Top-Slayer <dragontops55@gmail.com>
2026-07-06 04:51:10 +0700

    fix(neovim): restore same position of cursor and not be center of lines

--- nvim/lua/set.lua
+++ nvim/lua/set.lua
@@ -1,5 +1,35 @@
 vim.opt.scrolloff = 10
 
+local view_group = vim.api.nvim_create_augroup("PersistBufferView", { clear = true })
+
+vim.api.nvim_create_autocmd("BufLeave", {
+    group = view_group,
+    callback = function(args)
+        if vim.bo[args.buf].buftype ~= "" then
+            return
+        end
+
+        vim.b[args.buf].last_view = vim.fn.winsaveview()
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    group = view_group,
+    callback = function(args)
+        local view = vim.b[args.buf].last_view
+
+        if not view or vim.bo[args.buf].buftype ~= "" then
+            return
+        end
+
+        vim.schedule(function()
+            if vim.api.nvim_buf_is_valid(args.buf) and vim.api.nvim_get_current_buf() == args.buf then
+                pcall(vim.fn.winrestview, view)
+            end
+        end)
+    end,
+})
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 
